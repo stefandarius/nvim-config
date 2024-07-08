c6256af92ae20def005739b1548cab2e3e188461
Stefan Iordache <stefan.iordache2001@gmail.com>
2024-07-08 11:26:39 +0300

    trouble v3

--- lua/stefan/plugins/trouble.lua
+++ lua/stefan/plugins/trouble.lua
@@ -1,29 +1,38 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local trouble = require("trouble")
-		-- Lua
-		vim.keymap.set("n", "<leader>xx", function()
-			trouble.toggle()
-		end)
-		vim.keymap.set("n", "<leader>xw", function()
-			trouble.toggle("workspace_diagnostics")
-		end)
-		vim.keymap.set("n", "<leader>xd", function()
-			trouble.toggle("document_diagnostics")
-		end)
-		vim.keymap.set("n", "<leader>xq", function()
-			trouble.toggle("quickfix")
-		end)
-		vim.keymap.set("n", "<leader>xl", function()
-			trouble.toggle("loclist")
-		end)
-		vim.keymap.set("n", "]d", function()
-			trouble.next({ skip_groups = true, jump = true })
-		end)
-		vim.keymap.set("n", "[d", function()
-			trouble.previous({ skip_groups = true, jump = true })
-		end)
-	end,
+	opts = {},
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
 }
